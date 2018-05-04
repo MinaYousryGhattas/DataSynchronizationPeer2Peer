262f821c9fc3120d56e2d3556a33a6a226972ffd
Mina Yousry <menayousry60@hotmail.com>
2018-05-04 16:31:43 +0200

    Generator is thread now. and write his generated message to file..

--- src/WordGenerator.java
+++ src/WordGenerator.java
@@ -1,16 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by Mina_Yousry on 01/05/2018.
  */
-public class WordGenerator implements Generator {
+public class WordGenerator extends Thread implements Generator{
     ArrayList<String> words = new ArrayList<>(1000);
     Random random;
+    int counter = 0;
+    long sleepvalue = 10000;
+
     WordGenerator() throws IOException {
         random = new Random();
         BufferedReader in = new BufferedReader(new FileReader("1000Words.txt"));
@@ -31,5 +31,25 @@ public class WordGenerator implements Generator {
         sent += " " + words.get((random.nextInt(1000)));
         sent += " " + words.get( (random.nextInt(1000))) + ".";
         return sent;
+    }
+
+    public void run(){
+        boolean moredata = true;
+        while (moredata) {
+            try {
+                BufferedWriter out = new BufferedWriter(new FileWriter("outdata.txt"));
+                out.write(String.valueOf(counter++)+"\n");
+//                out.write(counter++);
+                out.append(generateData());
+                out.close();
+                System.out.println(counter-1);
+                sleep((long)sleepvalue);
+            } catch (IOException e) {
+                moredata = false;
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
